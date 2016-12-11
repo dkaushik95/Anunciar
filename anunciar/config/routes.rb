32b61455e9bdd19b65aa852c19ddb581e08c5dad
Dishant Kaushik <dkaushik95@gmail.com>
2016-12-11 17:07:11 +0530

    Added an API {V1}

--- anunciar/config/routes.rb
+++ anunciar/config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :announcements
   root 'announcements#index'
+  namespace :api, defaults: {format: 'json'} do
+  	namespace :v1 do
+  		resources :announcements
+  	end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
